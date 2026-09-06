d21731764916e24ed96bf58f2b21a2f57c362791
Owais <contact@owais.fi>
2026-09-06 16:13:17 +0300

    UPDATE: Add query parameters

--- internal/handler/movie.go
+++ internal/handler/movie.go
@@ -35,15 +35,40 @@ func (h *MovieHandler) Create(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *MovieHandler) GetAll(w http.ResponseWriter, r *http.Request) {
-	movies, err := h.service.GetAll()
+
+	var movies []models.Movie
+	var err error
+	
+	genreIDStr := r.URL.Query().Get("genre")
+	yearStr := r.URL.Query().Get("year")
+	actorIDStr := r.URL.Query().Get("actor")
+
+	if genreIDStr != "" {
+		genreID, _ := strconv.ParseInt(genreIDStr, 10, 64)
+		movies, err = h.service.GetByGenreID(genreID)
+	
+	} else if yearStr != "" {
+		year, _ := strconv.Atoi(yearStr)
+		movies, err = h.service.GetByReleaseYear(year)
+	
+	} else if actorIDStr != "" {
+		actorID, _ := strconv.ParseInt(actorIDStr, 10, 64)
+		movies, err = h.service.GetByActorID(actorID)
+	
+	} else {
+		// Default: fetch all movies
+		movies, err = h.service.GetAll()
+	}
+
 	if err != nil {
-		http.Error(w, "Failed to fetch movies", http.StatusInternalServerError)
+		http.Error(w, err.Error( ), http.StatusInternalServerError )
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(movies)
+
 }
 
 func (h *MovieHandler) GetByID(w http.ResponseWriter, r *http.Request) {
