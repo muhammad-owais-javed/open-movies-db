0729de0a90113e2ea6d6b6b3ac990a325215639b
Owais <contact@owais.fi>
2026-09-06 16:18:46 +0300

    UPDATE: Add New Endpoints

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -50,68 +50,6 @@ func main() {
 	actorHandler := handler.NewActorHandler(actorService)
 	movieHandler := handler.NewMovieHandler(movieService)
 
-	// fmt.Printf(">> TEST: Genre Repository Call...\n")
-
-	// newGenre := &models.Genre{
-	// 	Name: "Thriller",
-	// }
-
-	// fmt.Printf("\n--- Create Method ---\n")
-	// fmt.Printf("Initial Value > newGenre: %+v\n", newGenre)
-
-	// err = genreRepo.Create(newGenre)
-	// if err != nil {
-	// 	log.Printf("Error creating genre: %v\n", err)
-	// } else {
-	// 	fmt.Printf("Update Value > newGenre:  %+v\n", newGenre)
-	// }
-	// fmt.Printf("---------------------\n")
-
-	// fmt.Printf("\n--- GetAll Method ---\n")
-	// allGenres, err := genreRepo.GetAll()
-	// if err != nil {
-	// 	log.Printf("Error getting genre: %v\n", err)
-	// } else {
-	// 	fmt.Printf("Found %d genres: \n", len(allGenres))
-	// 	for _, g := range allGenres {
-	// 		fmt.Printf(" - ID: %d, Name: %s\n", g.ID, g.Name)
-	// 	}
-	// }
-	// fmt.Printf("---------------------\n")
-
-	// fmt.Printf("\n--- Update Method ---\n")
-	// genreToUpdate := &models.Genre{
-	// 	ID:   3,
-	// 	Name: "Horror",
-	// }
-	// err = genreRepo.Update(genreToUpdate)
-	// if err != nil {
-	// 	log.Printf("Error updating genre: %v\n", err)
-	// } else {
-	// 	fmt.Println("Successfully updated genre ID'!")
-	// }
-
-	// fmt.Printf("---------------------\n")
-
-	// fmt.Printf("\n--- Delete Method ---\n")
-	// err = genreRepo.Delete(3)
-	// if err != nil {
-	// 	log.Printf("Error deleting genre: %v\n", err)
-	// } else {
-	// 	fmt.Println("Successfully deleted genre ID!")
-	// }
-
-	// fmt.Printf("---------------------\n")
-
-	// fmt.Printf("\n--- Verification Method ---\n")
-	// genre, err := genreRepo.GetByID(2)
-	// if err != nil {
-	// 	fmt.Printf("Verification: %v\n", err)
-	// } else {
-	// 	fmt.Printf(" - ID: %d, Name: %s\n", genre.ID, genre.Name)
-	// }
-	// fmt.Printf("---------------------\n")
-
 	port := ":8080"
 	mux := http.NewServeMux()
 
@@ -133,18 +71,20 @@ func main() {
 	// Movies
 	mux.HandleFunc("POST /api/movies", movieHandler.Create)
 	mux.HandleFunc("GET /api/movies", movieHandler.GetAll)
-
-	// IMPORTANT: Keep it above {id} else it will break
-	mux.HandleFunc("GET /api/movies/search", movieHandler.Search)
-
 	mux.HandleFunc("GET /api/movies/{id}", movieHandler.GetByID)
+
+	mux.HandleFunc("GET /api/movies/search", movieHandler.Search)
+	mux.HandleFunc("GET /api/movies/{id}/actors", movieHandler.GetActors)
+
 	mux.HandleFunc("PATCH /api/movies/{id}", movieHandler.Update)
 	mux.HandleFunc("DELETE /api/movies/{id}", movieHandler.Delete)
 
 	fmt.Printf(">> Starting Server ...\n")
 	fmt.Printf(">> URL: http://localhost%s\n", port)
 
-	err = http.ListenAndServe(port, mux)
+	handlerWithMiddleware := PanicRecoveryMiddleware(mux)
+
+	err = http.ListenAndServe(port, handlerWithMiddleware )
 	if err != nil {
 		log.Fatalf(">> FATAL: Server Failed to start! \n%v", err)
 	}
